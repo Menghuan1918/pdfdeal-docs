6d4bdd3e28d94cfa1298d4d02a2e4c4c183cc970
Menghuan1918 <menghuan2003@outlook.com>
2024-07-10 19:55:00 +0800

    chore: 更新部署文档工作流程

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/pdfdeal-docs/",
 
   locales: {
     "/": {
